fd9a160496317093b014732b95b82e073f29fdff
Sven Herzberg <herzberg.sven@googlemail.com>
2012-04-20 19:25:10 +0200

    add "escaped" encoding
    * main.c: add the "escaped" encoding of Mac OS X

--- main.c
+++ main.c
@@ -11,56 +11,40 @@ int
 main (int   argc __attribute__((__unused__)),
       char**argv __attribute__((__unused__)))
 {
+	struct {
+		char* code;
+		char* file;
+	} tests[] = {
+		{
+			"Escaped\n", "hall%F6.txt"
+		}, {
+			"ASCII\n", "halloe.txt"
+		}, {
+			"ISO-8859-1\n", "hall\xf6.txt"
+		}, {
+			"UTF-8\n", "hall\xc3\xb6.txt"
+		}
+	};
     int fileDescriptor;
-    char* ascii = "ASCII\n";
-    char* iso88591 = "ISO-8859-1\n";
-    char* utf8 = "UTF-8\n";
-    size_t len;
+    ssize_t len;
+	size_t i;
 
-    fileDescriptor = creat("halloe.txt", 0644);
-    if (fileDescriptor < 0) {
-        perror("creat(ascii)");
-        return EXIT_FAILURE;
-    }
-    len = write(fileDescriptor, ascii, strlen(ascii));
-    if (len != strlen(ascii)) {
-        perror("write(ascii)");
-        return EXIT_FAILURE;
-    }
-    if (close(fileDescriptor) < 0) {
-        perror("close(ascii)");
-        return EXIT_FAILURE;
-    }
-
-    fileDescriptor = creat("hall\xf6.txt", 0644);
-    if (fileDescriptor < 0) {
-        perror("creat(iso88591)");
-        return EXIT_FAILURE;
-    }
-    len = write(fileDescriptor, iso88591, strlen(iso88591));
-    if (len != strlen(iso88591)) {
-        perror("write(iso88591)");
-        return EXIT_FAILURE;
-    }
-    if (close(fileDescriptor) < 0) {
-        perror("close(iso88591)");
-        return EXIT_FAILURE;
-    }
-
-    fileDescriptor = creat("hall\xc3\xb6.txt", 0644);
-    if (fileDescriptor < 0) {
-        perror("creat(utf8)");
-        return EXIT_FAILURE;
-    }
-    len = write(fileDescriptor, utf8, strlen(utf8));
-    if (len != strlen(utf8)) {
-        perror("write(utf8)");
-        return EXIT_FAILURE;
-    }
-    if (close(fileDescriptor) < 0) {
-        perror("close(utf8)");
-        return EXIT_FAILURE;
-    }
+	for (i = 0; i < sizeof(tests) / sizeof(*tests); i++) {
+		fileDescriptor = creat(tests[i].file, 0644);
+		if (fileDescriptor < 0) {
+			perror("creat()");
+			return EXIT_FAILURE;
+		}
+		len = write(fileDescriptor, tests[i].code, strlen(tests[i].code));
+		if (len < 0 || ((size_t)len) != strlen(tests[i].code)) {
+			perror("write()");
+			return EXIT_FAILURE;
+		}
+		if (close(fileDescriptor) < 0) {
+			perror("close()");
+			return EXIT_FAILURE;
+		}
+	}
 
     DIR* folder;
     folder = opendir(".");
@@ -79,10 +63,10 @@ main (int   argc __attribute__((__unused__)),
                 return EXIT_FAILURE;
             }
             strcpy(buf, entry->d_name);
-            
+
             fileDescriptor = open(buf, O_RDONLY);
             len = read(fileDescriptor, buf, sizeof(buf));
-            if (len >= sizeof(buf)) {
+            if (((size_t)len) >= sizeof(buf)) {
                 fprintf(stderr,
                         "the buffer is too small for the file: %ld bytes (increase to %ld)",
                         sizeof(buf), sizeof(buf) << 1);
